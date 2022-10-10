62f2af814350ac2aa561447be7d6ab2da1564d9e
Huy Nguyen <phuochuy.nguyen@shodan.io>
2022-10-10 22:12:51 +0700

    Upiiefawefafddd dddddddd

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 . ./chia-blockchain/activate
 chia init -c ca
+python edit_plotting_and_config_file.py
 chia start harvester
 mkdir "/root/.chia/mainnet/log"
 touch "/root/.chia/mainnet/log/debug.log"
 deactivate
-python edit_plotting_and_config_file.py
 python -u watcher.py
