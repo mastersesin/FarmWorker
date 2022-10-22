2e04bf74990b7427722b3d499ffb64de9f2f76ce
Huy Nguyen <phuochuy.nguyen@shodan.io>
2022-10-22 23:00:55 +0700

    Upiiefawefafddd dddddddd

--- run_docker.py
+++ run_docker.py
@@ -3,7 +3,7 @@ import subprocess
 project_name = subprocess.check_output('gcloud config get-value project'.split()).strip().decode()
 instance_name = subprocess.check_output('hostname'.split()).strip().decode()
 docker_id = f'{project_name}:{instance_name}'
-for i in range(1):
+for i in range(5):
     docker_cmd = f'sudo docker run -e WORKER_ID={docker_id}:host_{i} --log-driver=gcplogs ' \
                  f'--log-opt gcp-project=bright-seer-140205 ' \
                  f'--log-opt labels=worker_id --label worker_id={docker_id}:host_{i} ' \
