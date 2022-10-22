d985ed2e72524741a68d00ee68643dfcc0354a9a
Huy Nguyen <phuochuy.nguyen@shodan.io>
2022-10-22 23:06:59 +0700

    Upiiefawefafddd dddddddd

--- run_docker.py
+++ run_docker.py
@@ -3,6 +3,7 @@ import subprocess
 project_name = subprocess.check_output('gcloud config get-value project'.split()).strip().decode()
 instance_name = subprocess.check_output('hostname'.split()).strip().decode()
 docker_id = f'{project_name}:{instance_name}'
+subprocess.Popen('sudo docker build . -t farm-worker'.split(' '))
 for i in range(5):
     docker_cmd = f'sudo docker run -e WORKER_ID={docker_id}:host_{i} --log-driver=gcplogs ' \
                  f'--log-opt gcp-project=bright-seer-140205 ' \
